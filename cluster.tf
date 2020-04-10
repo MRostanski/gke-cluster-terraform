a785ad04782f6a6b13f9b8a6e28938ea66dce5d9
maciej <mrostanski@gmail.com>
2020-04-10 10:50:28 +0200

    initial

--- cluster.tf
+++ cluster.tf
@@ -1,29 +1,30 @@
-variable "general_purpose_machine_type" {
-  type = "string"
-  description = "Machine type to use for the general-purpose node pool. See https://cloud.google.com/compute/docs/machine-types"
-}
 
-variable "general_purpose_min_node_count" {
-  type = "string"
-  description = "The minimum number of nodes PER ZONE in the general-purpose node pool"
-  default = 1
-}
-
-variable "general_purpose_max_node_count" {
-  type = "string"
-  description = "The maximum number of nodes PER ZONE in the general-purpose node pool"
-  default = 5
-}
 
 resource "google_container_cluster" "cluster" {
-  name     = "${var.project}-cluster"
-  location = "${var.region}"
+  name        = "${var.project}-k8s"
+  location    = var.region
+  description = "Kubernetes cluster for ${var.project}"
 
   # We can't create a cluster with no node pool defined, but we want to only use
   # separately managed node pools. So we create the smallest possible default
   # node pool and immediately delete it.
   remove_default_node_pool = true
-  initial_node_count = 1
+  initial_node_count       = 1
+
+  #cluster_ipv4_cidr = var.cluster_ipv4_cidr
+
+  # VPC-native
+  ip_allocation_policy {
+    # cluster_secondary_range_name = # The name of the existing secondary range in the cluster's subnetwork to use for pod IP addresses.
+    # services_secondary_range_name - (Optional) The name of the existing secondary range in the cluster's subnetwork to use for service ClusterIPs. 
+    # Alternatively, services_ipv4_cidr_block can be used to automatically create a GKE-managed one.
+
+    cluster_ipv4_cidr_block  = "10.192.0.0/14"
+    services_ipv4_cidr_block = "10.196.0.0/14"
+  }
+
+  logging_service    = "logging.googleapis.com/kubernetes"
+  monitoring_service = "monitoring.googleapis.com/kubernetes"
 
   # Setting an empty username and password explicitly disables basic auth
   master_auth {
@@ -31,36 +32,79 @@ resource "google_container_cluster" "cluster" {
     password = ""
   }
 
+  master_authorized_networks_config {
+    cidr_blocks {
+      cidr_block   = "XXX.201.153.118"
+      display_name = "H"
+    }
+  }
+
   addons_config {
     network_policy_config {
       disabled = "false"
     }
+    horizontal_pod_autoscaling {
+      disabled = "false"
+    }
+    istio_config {
+      disabled = "false"
+    }
+    cloudrun_config {
+      disabled = "false"
+    }
+    dns_cache_config {
+      enabled = "true"
+    }
   }
 
+  # network = "" 
+  # subnetwork = ""
+
   network_policy {
-    enabled = "true"
+    enabled  = "true"
     provider = "CALICO"
+  }
+
+  # pod_security_policy_config {}
+
+  resource_labels = {
+    "Project" = var.project
+  }
+
+  workload_identity_config {}
+
+  maintenance_policy {
+    daily_maintenance_window {
+      start_time = "03:00"
+    }
   }
 }
 
 resource "google_container_node_pool" "general_purpose" {
-  name       = "${var.project}-general"
-  location   = "${var.region}"
-  cluster    = "${google_container_cluster.cluster.name}"
+  name     = "${var.project}-k8s-general"
+  location = var.region
+  cluster  = google_container_cluster.cluster.name
 
-  management { 
-    auto_repair = "true"
+  management {
+    auto_repair  = "true"
     auto_upgrade = "true"
   }
 
-  autoscaling { 
-    min_node_count = "${var.general_purpose_min_node_count}"
-    max_node_count = "${var.general_purpose_max_node_count}"
+  autoscaling {
+    min_node_count = var.general_purpose_min_node_count
+    max_node_count = var.general_purpose_max_node_count
   }
-  initial_node_count = "${var.general_purpose_min_node_count}"
+  initial_node_count = var.general_purpose_min_node_count
 
   node_config {
-    machine_type = "${var.general_purpose_machine_type}"
+    disk_size_gb = "20GB"
+    disk_type    = "pd-ssd"
+    machine_type = var.general_purpose_machine_type
+    # image-type
+
+    labels = {
+      "node-pool" = "general"
+    }
 
     metadata = {
       disable-legacy-endpoints = "true"
@@ -73,19 +117,24 @@ resource "google_container_node_pool" "general_purpose" {
       "https://www.googleapis.com/auth/monitoring",
       "https://www.googleapis.com/auth/devstorage.read_only"
     ]
+
+    preemptible = false
+
+    tags = ["${var.project}-k8s-general", "k8s-nodes"]
+
   }
 }
 
 # The following outputs allow authentication and connectivity to the GKE Cluster
 # by using certificate-based authentication.
 output "client_certificate" {
-  value = "${google_container_cluster.cluster.master_auth.0.client_certificate}"
+  value = google_container_cluster.cluster.master_auth.0.client_certificate
 }
 
 output "client_key" {
-  value = "${google_container_cluster.cluster.master_auth.0.client_key}"
+  value = google_container_cluster.cluster.master_auth.0.client_key
 }
 
 output "cluster_ca_certificate" {
-  value = "${google_container_cluster.cluster.master_auth.0.cluster_ca_certificate}"
+  value = google_container_cluster.cluster.master_auth.0.cluster_ca_certificate
 }
--- production.tfvars
+++
@@ -1,6 +0,0 @@
-project = "gke-from-scratch"
-region = "europe-north1"
-
-general_purpose_machine_type = "n1-standard-1"
-general_purpose_min_node_count = 1
-general_purpose_max_node_count = 3
